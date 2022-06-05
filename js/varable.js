1099e93a8c55ef362879b660512bf855a69478eb
ahmadsofuwan <sofuwan62@gmail.com>
2022-06-05 17:29:20 +0700

    update

---
+++ js/varable.js
@@ -0,0 +1,12 @@
+var myProject = [
+    {
+        title: 'project pertama',
+        img: 'gm1.jpg',
+        text: 'ini adalah project portama saya',
+    },
+    {
+        title: 'project kedua',
+        img: 'gm1.jpg',
+        text: 'ini adalah project kedua saya',
+    },
+]
