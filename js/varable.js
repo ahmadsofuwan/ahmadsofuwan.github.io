8a8e9ccbf0a670d5e74ff099da4760f674303965
ahmadsofuwan <sofuwan62@gmail.com>
2022-06-06 14:20:55 +0700

    update isi oneclick

--- js/varable.js
+++ js/varable.js
@@ -1,12 +1,13 @@
 var myProject = [
     {
         title: 'Gereja dangan notifikasi WhatsApp',
-        img: ['gereja.jpeg', 'gereja_dashboard.png', 'gereja_jadwal.png', 'gereja_laporan.png', 'gerjah_create jadwal.png'],
+        img: ['gereja.jpeg', 'gereja_dashboard.png', 'gereja_jadwal.png', 'gereja_laporan.png', 'gerjah_create jadwal.png', 'gereja_blash.png'],
         text: 'Aplikasi ini berfunsi sebagai aplikasi penjadwalan ibadah gerejah, yang di bantu blash WhastApp',
     },
     {
-        title: 'project kedua',
-        img: ['gm1.jpg', 'gm1.jpg'],
-        text: 'ini adalah project kedua saya',
+        title: 'Automation Aktifasi Beebeenet',
+        img: ['oneclick_aktifasi.png', 'oneclick_script.png', 'oneclick_tiket.png', 'oneclick_formLaporan.png'],
+        text: 'Aplikasi ini adalah Aplikasi untuk mendaftaran internet, baik melalui genrate script otomatis dan APi Aktifasi Otomatis dan didalamnya sudah terdapat tiket Sitem yang teringrasi dengan WhatsApp Bot <br>\
+        di Buat dengan bahasa pemograman PHP(CI3),javascript,boostrap4,APi mikrotik dan APi SSH',
     },
 ]
