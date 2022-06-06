b7ee262ff0b03ea3a38b7403a34fc2d3e1f07d96
ahmadsofuwan <sofuwan62@gmail.com>
2022-06-06 14:00:57 +0700

    update isi

--- js/varable.js
+++ js/varable.js
@@ -1,12 +1,12 @@
 var myProject = [
     {
-        title: 'project pertama',
-        img: 'gm1.jpg',
-        text: 'ini adalah project portama saya',
+        title: 'Gereja dangan notifikasi WhatsApp',
+        img: ['gereja.jpeg', 'gereja_dashboard.png', 'gereja_jadwal.png', 'gereja_laporan.png', 'gerjah_create jadwal.png'],
+        text: 'Aplikasi ini berfunsi sebagai aplikasi penjadwalan ibadah gerejah, yang di bantu blash WhastApp',
     },
     {
         title: 'project kedua',
-        img: 'gm1.jpg',
+        img: ['gm1.jpg', 'gm1.jpg'],
         text: 'ini adalah project kedua saya',
     },
 ]
