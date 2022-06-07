5d743ccf5371e6c5fa3cf5ba18b354d0de5b9de9
ahmadsofuwan <69912778+ahmadsofuwan@users.noreply.github.com>
2022-06-07 11:35:59 +0700

    update pesantren

--- js/varable.js
+++ js/varable.js
@@ -1,20 +1,36 @@
 var myProject = [
-    {
-        title: 'Gereja dangan notifikasi WhatsApp',
-        img: ['gereja.jpeg', 'gereja_dashboard.png', 'gereja_jadwal.png', 'gereja_laporan.png', 'gerjah_create jadwal.png', 'gereja_blash.png'],
-        text: 'Aplikasi ini berfunsi sebagai aplikasi penjadwalan ibadah gerejah, yang di bantu blash WhastApp<br>\
+  {
+    title: 'Gereja dangan notifikasi WhatsApp',
+    img: ['gereja.jpeg', 'gereja_dashboard.png', 'gereja_jadwal.png', 'gereja_laporan.png', 'gerjah_create jadwal.png', 'gereja_blash.png'],
+    text: 'Aplikasi ini berfunsi sebagai aplikasi penjadwalan ibadah gerejah, yang di bantu blash WhastApp<br>\
         Aplikasi ini di buat dengan php(CI3),javascript,boostrap4 dan terintegrasi dengan WhastApp Bot Nodejs',
-    },
-    {
-        title: 'Automation Aktifasi Beebeenet',
-        img: ['oneclick_aktifasi.png', 'oneclick_script.png', 'oneclick_tiket.png', 'oneclick_formLaporan.png'],
-        text: 'Aplikasi ini adalah Aplikasi untuk mendaftaran internet, baik melalui genrate script otomatis dan APi Aktifasi Otomatis dan didalamnya sudah terdapat tiket Sitem yang teringrasi dengan WhatsApp Bot <br>\
+  },
+  {
+    title: 'Automation Aktifasi Beebeenet',
+    img: ['oneclick_aktifasi.png', 'oneclick_script.png', 'oneclick_tiket.png', 'oneclick_formLaporan.png'],
+    text: 'Aplikasi ini adalah Aplikasi untuk mendaftaran internet, baik melalui genrate script otomatis dan APi Aktifasi Otomatis dan didalamnya sudah terdapat tiket Sitem yang teringrasi dengan WhatsApp Bot <br>\
         di Buat dengan bahasa pemograman PHP(CI3),javascript,boostrap4,APi mikrotik dan APi SSH',
-    },
-    {
-        title: 'WhastApp Bot',
-        img: ['whatsApp_logo.png', 'whatsApp_chat.PNG', 'whatsApp_menu.PNG', 'whatsApp_tiket.PNG'],
-        text: 'Aplikasi WhatsApp Bot ini bisa di gunakan sebagai Api untuk Blash Dan bisa juga sebagai Bot yang bisa membalas pesan Otomatis sesuai keinginan<br>\
+  },
+  {
+    title: 'WhastApp Bot',
+    img: ['whatsApp_logo.png', 'whatsApp_chat.PNG', 'whatsApp_menu.PNG', 'whatsApp_tiket.PNG'],
+    text: 'Aplikasi WhatsApp Bot ini bisa di gunakan sebagai Api untuk Blash Dan bisa juga sebagai Bot yang bisa membalas pesan Otomatis sesuai keinginan<br>\
         Aplikasi di buat dengan Full javscript dengan Nodejs sebagai servernya',
-    },
-]
+  },
+  {
+    title: 'Aplikasi Pencatatan Hafalan',
+    img: [
+      'pesantren_dashboard.PNG',
+      'pesantren_import.png',
+      'pesantren_inputClass.png',
+      'pesantren_listClass.png',
+      'pesantren_inputHafalan.png',
+      'pesantren_listHafalan.png',
+      'pesantren_inputLevel.png',
+      'pesantren_lisLevel.png',
+      'pesantren_inputSiswa.png',
+    ],
+    text: 'aplikasi ini untuk mencatat hafalan siswa, terdapat 2 role superadmin dan guru, hafalan sudah di buat secara dinamis, ada group hafalan, class dan guru<br>\
+    di buat dengan php(CI3),javascript, bostrap4 jquery dan mysql',
+  },
+];
